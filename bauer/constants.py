436d748cd17b3d06db2f702313f058eb28cfdf2a
Endogen <081081@gmx.net>
2019-09-02 22:50:15 +0200

    Dynamic generation of source path

--- bauer/constants.py
+++ bauer/constants.py
@@ -1,15 +1,17 @@
+import os
+
+# Project folders
+DIR_SRC = os.path.basename(os.path.dirname(__file__))
+DIR_RES = "resources"
+DIR_PLG = "plugins"
+DIR_CFG = "config"
+DIR_LOG = "logs"
+DIR_DAT = "data"
+
 # Project files
 FILE_CFG = "config.json"
 FILE_TKN = "token.json"
 FILE_LOG = "bauer.log"
-
-# Project folders
-DIR_RES = "resources"
-DIR_PLG = "plugins"
-DIR_CFG = "config"
-DIR_SRC = "bauer"
-DIR_LOG = "logs"
-DIR_DAT = "data"
 
 # Max Telegram message length
 MAX_TG_MSG_LEN = 4096
