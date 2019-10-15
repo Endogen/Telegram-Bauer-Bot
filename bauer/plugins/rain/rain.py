56d8ff35f63f2a43dd818b363ff1245e92fbd73b
endogen <ramodroll>
2019-10-15 10:23:00 +0200

    [DO NOT MERGE!] Work in progress...

--- bauer/plugins/rain/rain.py
+++ bauer/plugins/rain/rain.py
@@ -75,87 +75,89 @@ class Rain(BauerPlugin):
         from_user = update.effective_user.username
         from_user_id = update.effective_user.id
 
+        # Check if sender has a wallet
+        if not Bismuth.wallet_exists(from_user):
+            msg = "Accept terms and create a wallet first with:\n/accept"
+            update.message.reply_text(msg)
+            return
+
         sql = self.get_resource("read_users.sql")
         res = self.execute_sql(sql, plugin="wallet")
 
-        if res["success"]:
-            # Remove own user from list of users to rain on
-            user_list = [x for x in res["data"] if x[1] != from_user]
-
-            if len(user_list) < users:
-                msg = f"{emo.ERROR} Not enough users. {len(user_list)} available"
-                update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
-                return
-
-            # Randomly choose users from all users
-            chosen = random.sample(user_list, users)
-
-            # Check if sender has a wallet
-            if not Bismuth.wallet_exists(from_user):
-                msg = "Accept terms and create a wallet first with:\n/accept"
-                update.message.reply_text(msg)
-                return
-
-            msg = f"{emo.WAIT} Processing..."
-            message = update.message.reply_text(msg)
-
-            # Init sender wallet
-            bis = Bismuth(from_user)
-            bis.load_wallet()
-
-            # Check for sufficient funds
-            if float(bis.get_balance()) <= amount:
-                msg = f"{emo.ERROR} Not enough funds"
-                update.message.reply_text(msg)
-                return
-
-            result = f"{emo.DONE} @{utl.esc_md(from_user)} sent `{user_amount}` each to: "
-
-            for to_data in chosen:
-                to_user_id = to_data[0]
-                to_user = to_data[1]
-
-                # Execute tipping
-                if bis.tip(to_user, user_amount):
-                    # Save tipping in database
-                    # TODO: Do i have to lock the DB while writing?
-                    insert = self.get_resource("insert_rain.sql")
-                    self.execute_sql(insert, from_user, to_user, amount)
-
-                    # Add username of tipped user to confirmation message
-                    result += f"@{utl.esc_md(to_user)} "
-
-                    # Send message to tipped user
-                    msg = f"You've been tipped with `{amount}` BIS by @{utl.esc_md(from_user)}"
-                    bot.send_message(to_user_id, msg, parse_mode=ParseMode.MARKDOWN)
-                else:
-                    # Send error message to tipping user
-                    msg = f"{emo.ERROR} Sending `{amount}` to @{utl.esc_md(to_user)} not possible. Check logs."
-                    bot.send_message(from_user_id, msg, parse_mode=ParseMode.MARKDOWN)
-
-                    logging.error(msg)
-                    self.notify(msg)
-
-            # Check if at least one user got tipped
-            if result.count("@") > 1:
-                # TODO: Use 'split_msg' from utils here
-                # Send success message
-                bot.edit_message_text(
-                    chat_id=message.chat_id,
-                    message_id=message.message_id,
-                    text=result,
-                    parse_mode=ParseMode.MARKDOWN)
-            else:
-                # Send error message
-                bot.edit_message_text(
-                    chat_id=message.chat_id,
-                    message_id=message.message_id,
-                    text=f"{emo.ERROR} Rain not executed. Something went wrong...",
-                    parse_mode=ParseMode.MARKDOWN)
-        else:
+        if not res["success"]:
             msg = f"{emo.ERROR} Not possible to retrieve users"
             update.message.reply_text(msg)
 
             error = res["data"]
             logging.error(error)
             self.notify(error)
+            return
+
+        # Remove own user from list of users to rain on
+        user_list = [x for x in res["data"] if x[1] != from_user]
+
+        # Check if enough user available to be rained on
+        if len(user_list) < users:
+            msg = f"{emo.ERROR} Not enough users. {len(user_list)} available"
+            update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+            return
+
+        # Randomly choose users from all users
+        chosen = random.sample(user_list, users)
+
+        msg = f"{emo.WAIT} Processing..."
+        message = update.message.reply_text(msg)
+
+        # Init sender wallet
+        bis = Bismuth(from_user)
+        bis.load_wallet()
+
+        # Check for sufficient funds
+        if float(bis.get_balance()) <= amount:
+            msg = f"{emo.ERROR} Not enough funds"
+            update.message.reply_text(msg)
+            return
+
+        result = f"{emo.DONE} @{utl.esc_md(from_user)} sent `{user_amount}` each to: "
+
+        for to_data in chosen:
+            to_user_id = to_data[0]
+            to_user = to_data[1]
+
+            # Execute tipping
+            if bis.tip(to_user, user_amount):
+                # Save tipping in database
+                # TODO: Do i have to lock the DB while writing?
+                insert = self.get_resource("insert_rain.sql")
+                self.execute_sql(insert, from_user, to_user, amount)
+
+                # Add username of tipped user to confirmation message
+                result += f"@{utl.esc_md(to_user)} "
+
+                # Send message to tipped user
+                msg = f"You've been tipped with `{user_amount}` BIS by @{utl.esc_md(from_user)}"
+                bot.send_message(to_user_id, msg, parse_mode=ParseMode.MARKDOWN)
+            else:
+                # Send error message to tipping user
+                msg = f"{emo.ERROR} Sending `{user_amount}` to @{utl.esc_md(to_user)} not possible. Check logs."
+                bot.send_message(from_user_id, msg, parse_mode=ParseMode.MARKDOWN)
+
+                logging.error(msg)
+                self.notify(msg)
+
+        # Check if at least one user got tipped
+        if result.count("@") > 1:
+            # TODO: Use 'split_msg' from utils here
+            # Send success message
+            bot.edit_message_text(
+                chat_id=message.chat_id,
+                message_id=message.message_id,
+                text=result,
+                parse_mode=ParseMode.MARKDOWN)
+        else:
+            # Send error message
+            bot.edit_message_text(
+                chat_id=message.chat_id,
+                message_id=message.message_id,
+                text=f"{emo.ERROR} Rain not executed. Something went wrong...",
+                parse_mode=ParseMode.MARKDOWN)
